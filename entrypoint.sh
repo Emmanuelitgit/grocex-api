64388b7dfcbf2342d92aee7a1429880504d77b06
Emmanuel Yidana <EmmanuelYi@stlghana.com>
2025-05-02 10:33:30 +0000

    railway deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Strip quotes from Railway-provided env variables
+# Strip surrounding quotes from Railway env vars
 CLEAN_DATABASE_URL=$(echo $DATABASE_URL | sed 's/^"\(.*\)"$/\1/')
 CLEAN_DATABASE_USERNAME=$(echo $DATABASE_USERNAME | sed 's/^"\(.*\)"$/\1/')
 CLEAN_DATABASE_PASSWORD=$(echo $DATABASE_PASSWORD | sed 's/^"\(.*\)"$/\1/')
 
-# Export clean values to Spring Boot
-export SPRING_DATASOURCE_URL=$CLEAN_DATABASE_URL
-export SPRING_DATASOURCE_USERNAME=$CLEAN_DATABASE_USERNAME
-export SPRING_DATASOURCE_PASSWORD=$CLEAN_DATABASE_PASSWORD
+# Optional logging for sanity check (do not log password!)
+echo "Using DB URL: $CLEAN_DATABASE_URL"
+echo "Using DB Username: $CLEAN_DATABASE_USERNAME"
 
-# Start the app
-exec java -jar app.jar
+# Run Spring Boot app with cleaned DB config
+exec java \
+  -Dspring.datasource.url=$CLEAN_DATABASE_URL \
+  -Dspring.datasource.username=$CLEAN_DATABASE_USERNAME \
+  -Dspring.datasource.password=$CLEAN_DATABASE_PASSWORD \
+  -jar app.jar
